77ee1a95687f963db7f96b2e91d3ff46675d1f6e
PVladimirS <volodia.pavloff201343@gmail.com>
2023-06-30 09:08:00 +0300

    Added create hw DOM, the task one and three

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,88 @@
+import { DataOne } from "./sorted_data_one.js";
+import { Gnom } from "./gnoms.js";
+
 // TODO: write code here
+document.addEventListener("DOMContentLoaded", () => {
+  const dataSet = [
+    {
+      id: 26,
+      title: "Побег из Шоушенка",
+      imdb: 9.3,
+      year: 1994,
+    },
+    {
+      id: 25,
+      title: "Крёстный отец",
+      imdb: 9.2,
+      year: 1972,
+    },
+    {
+      id: 27,
+      title: "Крёстный отец 2",
+      imdb: 9.0,
+      year: 1974,
+    },
+    {
+      id: 1047,
+      title: "Тёмный рыцарь",
+      imdb: 9.0,
+      year: 2008,
+    },
+    {
+      id: 223,
+      title: "Криминальное чтиво",
+      imdb: 8.9,
+      year: 1994,
+    },
+  ];
+
+  const gnom = new Gnom(document.querySelector(".table"));
+  const dataOne = new DataOne();
+  window.gnom = gnom;
+  window.dataOne = dataOne;
+
+  let lastIndex = 0;
+
+  const gnomsInterval = setInterval(() => {
+    function getRundomInt(max) {
+      return Math.floor(Math.random() * (max - 1));
+    }
+
+    let sectionNumber = getRundomInt(4);
+    if (lastIndex !== sectionNumber) {
+      gnom.addGnom(sectionNumber);
+      gnom.removeGnom(lastIndex);
+      lastIndex = sectionNumber;
+    }
+  }, 1000);
+
+  const dataFields = ["id", "title", "year", "imdb"];
+  dataOne.creteHeader();
+  let fieldCounter = 0;
+  let sortSide = true;
+  const tableInterval = setInterval(() => {
+    dataOne.clearTable();
+    if (sortSide) {
+      const sortDown = dataOne.sortedData(dataSet, dataFields[fieldCounter]);
+      dataOne.insertTable(sortDown);
+      sortSide = false;
+    } else {
+      const sortTop = dataOne.sortedDataReverse(
+        dataSet,
+        dataFields[fieldCounter]
+      );
+      dataOne.insertTable(sortTop);
+      sortSide = true;
+      fieldCounter++;
+      if (fieldCounter === dataFields.length) {
+        fieldCounter = 0;
+      }
+    }
+  }, 3000);
+});
 
 // comment this to pass build
-const unusedVariable = "variable";
+// const unusedVariable = "variable";
 
 // for demonstration purpose only
 export default function demo(value) {
---
+++ src/js/sorted_data_one.js
@@ -0,0 +1,105 @@
+export class DataOne {
+
+  creteHeader() {
+    const tableDiv = document.querySelector(".sorted_one");
+    const trHeader = document.createElement("tr");
+    const thId = document.createElement("th");
+    const span = document.createElement("span");
+    const thTitle = document.createElement("th");
+    const thImdb = document.createElement("th");
+    const thYear = document.createElement("th");
+    thId.textContent = "id";
+    thTitle.textContent = "title";
+    thYear.textContent = "year";
+    thImdb.textContent = "imdb";
+    tableDiv.append(trHeader);
+    trHeader.append(thId);
+    thId.append(span);
+    trHeader.append(thTitle);
+    trHeader.append(thYear);
+    trHeader.append(thImdb);
+  }
+
+  insertTable(dataSet) {
+    const tableDiv = document.querySelector(".sorted_one");
+    for (let data of dataSet) {
+      const tr = document.createElement("tr");
+      tr.setAttribute("data-id", data.id);
+      tr.setAttribute("data-title", data.title);
+      tr.setAttribute("data-imdb", data.imdb);
+      tr.setAttribute("data-year", data.year);
+      tableDiv.append(tr);
+      let tableElem = tableDiv.querySelectorAll("tr");
+      tableElem = tableElem[tableElem.length - 1];
+      const tdId = document.createElement("td");
+      const tdTitle = document.createElement("td");
+      const tdImdb = document.createElement("td");
+      const tdYear = document.createElement("td");
+      tdId.textContent = `#${tableElem.dataset.id}`;
+      tdTitle.textContent = `${tableElem.dataset.title}`;
+      tdYear.textContent = `(${tableElem.dataset.year})`;
+      tdImdb.textContent = `Imdb: ${Number(tableElem.dataset.imdb).toFixed(2)}`;
+      tableElem.append(tdId);
+      tableElem.append(tdTitle);
+      tableElem.append(tdYear);
+      tableElem.append(tdImdb);
+    }
+  }
+
+  sortedData(dataSet, field) {
+    const tableDiv = document.querySelector(".sorted_one");
+    let tableHeaders = tableDiv.querySelectorAll("tr")[0];
+    for (let header of tableHeaders.querySelectorAll("th")) {
+      if (header.textContent === field) {
+        header.classList.add("arrow_down");
+      }
+    }
+    dataSet.sort(function (a, b) {
+      if (a[field] > b[field]) {
+        return 1;
+      }
+      if (a[field] < b[field]) {
+        return -1;
+      }
+      return 0;
+    });
+    return dataSet;
+  }
+
+  sortedDataReverse(dataSet, field) {
+    const tableDiv = document.querySelector(".sorted_one");
+    let tableHeaders = tableDiv.querySelectorAll("tr")[0];
+    for (let header of tableHeaders.querySelectorAll("th")) {
+      if (header.textContent === field) {
+        header.classList.add("arrow_top");
+      }
+    }
+    dataSet.sort(function (a, b) {
+      if (a[field] < b[field]) {
+        return 1;
+      }
+      if (a[field] > b[field]) {
+        return -1;
+      }
+      return 0;
+    });
+    return dataSet;
+  }
+
+  clearTable() {
+    const tableDiv = document.querySelector(".sorted_one");
+    let tableElem = tableDiv.querySelectorAll("tr");
+    let count = 0;
+    for (let elem of tableElem) {
+      if (count !== 0) {
+        elem.remove();
+      } else {
+        const thAll = elem.querySelectorAll("th");
+        for (let th of thAll) {
+          th.classList.remove("arrow_down", "arrow_top");
+        }
+      }
+      count++;
+    }
+  }
+}
